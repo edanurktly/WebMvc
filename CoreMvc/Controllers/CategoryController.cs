e1e1783cf503f03e9c5c125acb14eb57028c107b
edanurktly <kutluayedanur@gmail.com>
2022-12-08 20:13:34 +0300

    08.12.2022

---
+++ CoreMvc/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using CoreMvc.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreMvc.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly NorthwindContext context;
+        public CategoryController(NorthwindContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            var result = context.Categories.ToList();
+            return View(result);
+        }
+        public IActionResult CategoryorProducts()
+        {
+
+
+            return View();
+        }
+    }
+}
---
+++ CoreMvc/Controllers/ShipperController.cs
@@ -0,0 +1,99 @@
+﻿using CoreMvc.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreMvc.Controllers
+{
+    public class ShipperController : Controller
+    {
+        private readonly NorthwindContext context;
+
+        public ShipperController(NorthwindContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Index()
+        {
+
+            var result = context.Shippers.ToList();
+            return View(result);
+        }
+
+        public IActionResult ShipperorOrders()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            var shipper = new Shipper();
+            return View(shipper);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Shipper shipper)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(shipper);
+            }
+
+            context.Shippers.Add(shipper);
+
+            int sonuc = context.SaveChanges();
+            if (sonuc > 0)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(shipper);
+        }
+
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            var shipper = context.Shippers.Find(id);
+            return View(shipper);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Shipper? shipper)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return View(shipper);
+
+            }
+            context.Shippers.Update(shipper);
+            int sonuc = context.SaveChanges();
+            if (sonuc > 0)
+            {
+                return RedirectToAction("Index", "Shipper");
+            }
+            return View(shipper);
+        }
+
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var shipper = context.Shippers.Find(id);
+            return View(shipper);
+        }
+        [HttpPost]
+        public IActionResult Delete(Shipper? shipper)
+        {
+
+
+            context.Shippers.Remove(shipper);
+            int sonuc = context.SaveChanges();
+            if (sonuc > 0)
+            {
+                return RedirectToAction("Index", "Shipper");
+            }
+            return View(shipper);
+        }
+
+    }
+}
